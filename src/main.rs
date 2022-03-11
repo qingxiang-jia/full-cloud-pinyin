fff883b7eaf95b8f07f5ae0a2f42fc90c095a781
Qingxiang Jia <ybjqx3340@gmail.com>
2022-03-10 20:00:23 -0500

    Use reqwest for simplicity

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::{io::{Write, stdout, stdin}, time::Instant, str::FromStr};
-use hyper_tls::HttpsConnector;
-use hyper::{Client, Uri, client::{HttpConnector, ResponseFuture}};
+use std::{io::{Write, stdout, stdin}, time::Instant};
 
-#[tokio::main]
-async fn main() {
-    let https = HttpsConnector::new();
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
-
+fn main() {
+    let client = reqwest::blocking::Client::new();
     loop {
         let mut input = String::new();
         let _ = stdout().flush();
@@ -15,35 +9,21 @@ async fn main() {
         
         let now = Instant::now();
 
-        let future = get_candidates(&input, &client);
-        let result = future.await;
-        match result {
-            Ok(response) => {
-                let data = hyper::body::to_bytes(response.into_body()).await;
-                
-                match data {
-                    Ok(buf) => {
-                        let maybe_str = std::str::from_utf8(&buf);
-                        
-                        match maybe_str {
-                            Ok(str) => println!("{:#?}", str),
-                            Err(err) => println!("Failed to convert bytes to str: {:#?}", err)
-                        }
-                    },
-                    Err(err) => println!("Reading body failed: {:#?}", err)
-                }
-            },
-            Err(err) => println!("{:#?}", err)
-        }
+        let response = get_candidates(&input, &client);
+        println!("{:#?}", response);
 
         let elapsed = now.elapsed();
         println!("{:#?}", elapsed);
     }
 }
 
-fn get_candidates(pinyin: &str, client: &Client<hyper_tls::HttpsConnector<HttpConnector>, hyper::Body>) -> ResponseFuture {
-    let url = format!("https://inputtools.google.com/request?text={}&itc=zh-t-i0-pinyin&num=11&cp=0&cs=1&ie=utf-8&oe=utf-8", pinyin.strip_suffix('\n').unwrap());
-    println!("request URL: {}", url);
-    let future = client.get(Uri::from_str(&url).expect("Invalid URI"));
-    return future;
+fn get_candidates(pinyin: &str, client: &reqwest::blocking::Client) -> String {
+    let data = client.get(
+        format!("https://inputtools.google.com/request?text={}&itc=zh-t-i0-pinyin&num=11&cp=0&cs=1&ie=utf-8&oe=utf-8",
+        pinyin.strip_suffix('\n').expect("Nothing to return after stirpping.")))
+        .send()
+        .expect("Network problems.")
+        .text()
+        .expect("The data cannot be converted to string.");
+    data
 }
