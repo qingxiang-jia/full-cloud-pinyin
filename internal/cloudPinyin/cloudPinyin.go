d3ad0a1ba69e377418de6c847a16c6d46089c988
Qingxiang Jia <ybjqx3340@gmail.com>
2022-08-16 06:46:20 -0400

    Implement cloud part

---
+++ internal/cloudPinyin/cloudPinyin.go
@@ -0,0 +1,81 @@
+package cloudpinyin
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"time"
+
+	"golang.org/x/net/http2"
+)
+
+var re, _ = regexp.Compile("[^\"\\[\\],{}}]+")
+
+const maxPinyinLen = 50
+
+type CloudPinyin struct {
+	conditioned string
+	http2       http.Client
+}
+
+// https://posener.github.io/http2/
+
+func (c *CloudPinyin) New() *CloudPinyin {
+	client := http.Client{Timeout: 5 * time.Second}
+	client.Transport = &http2.Transport{}
+
+	cp := CloudPinyin{"", client}
+	return &cp
+}
+
+func (c *CloudPinyin) GetCandidates(pinyin string, candCnt int) ([]string, error) {
+	if len(pinyin) > maxPinyinLen {
+		pinyin = pinyin[:50]
+	}
+
+	url := fmt.Sprintf("https://inputtools.google.com/request?text=%s&itc=zh-t-i0-pinyin&num=%d&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage", pinyin, candCnt)
+
+	// ["SUCCESS",[["ceshi",["测试","策士","侧视","测","侧","册","策","厕","CE","恻","測"],[],{"annotation":["ce shi","ce shi","ce shi","ce","ce","ce","ce","ce","c e","ce","ce"],"candidate_type":[0,0,0,0,0,0,0,0,0,0,0],"lc":["16 16","16 16","16 16","16","16","16","16","16","0 0","16","16"],"matched_length":[5,5,5,2,2,2,2,2,2,2,2]}]]]
+	fmt.Println(url)
+
+	resp, err := c.http2.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	str := string(data)
+
+	cand, err := jstrToCand(str)
+
+	return cand, err
+}
+
+func jstrToCand(jstr string) ([]string, error) {
+	words := re.FindAllString(jstr, -1)
+
+	fmt.Println(words)
+
+	cand := []string{}
+	for i, val := range words {
+		if i == 0 {
+			if val != "SUCCESS" {
+				return nil, errors.New("network request failed")
+			}
+		}
+		if val == "annotation" {
+			break
+		} else {
+			cand = append(cand, val)
+		}
+	}
+	return cand, nil
+}
---
+++ internal/consts/const.go
@@ -0,0 +1,58 @@
+package consts
+
+const (
+	CandCntA = 11
+	CandCntB = 21
+	CandCntC = 41
+	CandCntD = 81
+	CandCntE = 161
+	CandCntF = 321
+	CandCntG = 641
+	CandCntH = 1281
+)
+
+const (
+	IBUS_a = iota + 97
+	IBUS_b
+	IBUS_c
+	IBUS_d
+	IBUS_e
+	IBUS_f
+	IBUS_g
+	IBUS_h
+	IBUS_i
+	IBUS_j
+	IBUS_k
+	IBUS_l
+	IBUS_m
+	IBUS_n
+	IBUS_o
+	IBUS_p
+	IBUS_q
+	IBUS_r
+	IBUS_s
+	IBUS_t
+	IBUS_u
+	IBUS_v
+	IBUS_w
+	IBUS_x
+	IBUS_y
+	IBUS_z
+)
+
+const (
+	IBUS_BACKSPACE = 65288
+	IBUS_UP        = 65362
+	IBUS_DOWN      = 65364
+	IBUS_LEFT      = 65361
+	IBUS_RIGHT     = 65363
+	IBUS_ENTER     = 65293
+	IBUS_TILDA     = 65239
+	IBUS_SHIFT_L   = 65505
+	IBUS_SHIFT_R   = 65506
+	IBUS_ALT_L     = 65513
+	IBUS_ALT_R     = 65514
+	IBUS_CTRL_L    = 65507
+	IBUS_CTRL_R    = 65508
+	IBUS_ESC       = 65307
+)
