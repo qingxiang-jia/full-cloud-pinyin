0186df71f55f060c01bf774b2a54efca9b859e43
Qingxiang Jia <ybjqx3340@gmail.com>
2022-08-25 09:04:39 -0400

    Add a thin abstraction on top of LookupTable

---
+++ internal/engine/table.go
@@ -0,0 +1,89 @@
+package engine
+
+import "github.com/haunt98/goibus/ibus"
+
+type CandidateTable struct {
+	lt *ibus.LookupTable
+	e  *ibus.Engine
+}
+
+func NewCandidateTbale(e *ibus.Engine) *CandidateTable {
+	return &CandidateTable{
+		lt: ibus.NewLookupTable(),
+		e:  e,
+	}
+}
+
+func (t *CandidateTable) Add(candidates []string) {
+	for _, val := range candidates {
+		t.lt.AppendCandidate(val)
+	}
+}
+
+func (t *CandidateTable) Clear() {
+	t.lt.Candidates = t.lt.Candidates[:0]
+	t.lt.Labels = t.lt.Labels[:0]
+}
+
+func (t *CandidateTable) Hide() {
+	t.e.HideLookupTable()
+}
+
+func (t *CandidateTable) Show() {
+	t.e.ShowLookupTable()
+}
+
+// Not sure why the buil-in cursor moving functions don't work so I need to write my own.
+// Same for the next three.
+func (t *CandidateTable) CursorDown() bool {
+	if t.lt.CursorPos == 0 {
+		return false
+	}
+	t.lt.CursorPos--
+	return true
+}
+
+func (t *CandidateTable) CursorUp() bool {
+	if int(t.lt.CursorPos) == len(t.lt.Candidates) {
+		return false
+	}
+	t.lt.CursorPos++
+	return true
+}
+
+func (t *CandidateTable) PageDown() {
+	sz := t.lt.PageSize
+	pos := t.lt.CursorPos
+	if pos < sz {
+		return
+	}
+	pos -= sz
+	t.lt.CursorPos = pos
+	t.e.UpdateLookupTable(t.lt, true)
+}
+
+func (t *CandidateTable) PageUp() {
+	sz := int(t.lt.PageSize)
+	total := len(t.lt.Candidates)
+	nextPos := int(t.lt.CursorPos)
+	nextPos += sz
+	if nextPos >= total {
+		nextPos = total - 1
+	}
+	if nextPos != int(t.lt.CursorPos) {
+		t.lt.CursorPos = uint32(nextPos)
+		t.e.UpdateLookupTable(t.lt, true)
+	}
+}
+
+func (t *CandidateTable) AtLastPage() bool {
+	sz := int(t.lt.PageSize)
+	total := len(t.lt.Candidates)
+	maxIdx := (total/sz+1)*sz - 1
+	curIdx := int(t.lt.CursorPos)
+	if maxIdx-curIdx < sz {
+		return true
+	} else {
+		return false
+	}
+}
