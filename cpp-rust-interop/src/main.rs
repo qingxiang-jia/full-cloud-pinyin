4114eaf25f505f6a2c7a6b678318e049b0a151f4
Qingxiang Jia <ybjqx3340@gmail.com>
2022-09-11 10:10:57 -0400

    Play with async

--- cpp-rust-interop/src/main.rs
+++ cpp-rust-interop/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::time::{sleep, Duration};
+
 #[cxx::bridge]
 mod ffi {
     unsafe extern "C++" {
@@ -6,6 +8,15 @@ mod ffi {
     }
 }
 
-fn main() {
-    ffi::print("hey hey hey".to_string());
+async fn late_print() {
+    sleep(Duration::from_millis(3000)).await;
+    println!("2: I am good now");
+}
+
+#[tokio::main]
+async fn main() {
+    // ffi::print("hey hey hey".to_string());
+    println!("1");
+    late_print().await;
+    println!("3");
 }
