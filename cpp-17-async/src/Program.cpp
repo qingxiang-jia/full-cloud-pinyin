7788c5da112f0ff460af9f8f395043e21ac4e2f9
Qingxiang Jia <ybjqx3340@gmail.com>
2022-09-11 21:11:22 -0400

    Add some fake heavy operation

--- cpp-17-async/src/Program.cpp
+++ cpp-17-async/src/Program.cpp
@@ -1,8 +1,16 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+void longOperation() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::cout << "Long operation is completed.\n";
+}
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!\n";
+    longOperation();
     return 0;
 }
