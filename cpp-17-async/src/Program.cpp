be91de0e088fd5dbc47f5b285f1fa3dc3beac15c
Qingxiang Jia <ybjqx3340@gmail.com>
2022-09-13 07:42:01 -0400

    Async and friends (such as packaged_task) will block unless we save the returned future

--- cpp-17-async/src/Program.cpp
+++ cpp-17-async/src/Program.cpp
@@ -1,16 +1,31 @@
 // Your First C++ Program
 
-#include <iostream>
 #include <chrono>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <new>
 #include <thread>
 
-void longOperation() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    std::cout << "Long operation is completed.\n";
+void longOperation(std::string input) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+    std::cout << "You typed: " << input << "\n";
 }
 
 int main() {
-    std::cout << "Hello World!\n";
-    longOperation();
+    std::string in;
+
+    std::cout << "q to quit, empty input to continue\n";
+
+    while (true) {
+        std::cout << "say:\n";
+        std::cin >> in;
+        if (in == "q") {
+            return 0;
+        }
+        std::thread t(longOperation, in);
+        t.detach();
+        std::cout << "next line after async\n";
+    }
     return 0;
 }
