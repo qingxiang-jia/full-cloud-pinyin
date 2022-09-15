e4571f37c3519d928b4fcc3e867258086772200e
Qingxiang Jia <ybjqx3340@gmail.com>
2022-09-15 08:25:47 -0400

    Make fire and forget async since async is much cheaper than a thread

--- cpp-17-async/src/Program.cpp
+++ cpp-17-async/src/Program.cpp
@@ -1,15 +1,32 @@
-// Your First C++ Program
-
 #include <chrono>
 #include <future>
 #include <iostream>
 #include <memory>
 #include <new>
 #include <thread>
+#include <functional>
+
+
+
+template <class F, typename... Args>
+void call_async(F&& fun) {
+    // https://stackoverflow.com/a/56834117/1509779
+    auto futptr = std::make_shared<std::future<void>>();
+    *futptr = std::async(std::launch::async, [futptr, fun]() {
+        fun();
+    });
+}
 
 void longOperation(std::string input) {
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
     std::cout << "You typed: " << input << "\n";
+}
+
+void fireAndForgetLongOp(std::string input, std::function<void (std::string)> fn) {
+    auto futPtr = std::make_shared<std::future<void>>();
+    *futPtr = std::async(std::launch::async, [futPtr, fn, input]() {
+        fn(input);
+    });
 }
 
 int main() {
@@ -23,8 +40,7 @@ int main() {
         if (in == "q") {
             return 0;
         }
-        std::thread t(longOperation, in);
-        t.detach();
+        fireAndForgetLongOp(in, longOperation);
         std::cout << "next line after async\n";
     }
     return 0;
