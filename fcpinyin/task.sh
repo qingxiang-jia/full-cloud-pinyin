5043160db8126aa6272838699600a7e2e949caaa
Qingxiang Jia <ybjqx3340@gmail.com>
2022-10-01 08:49:52 -0400

    Warn what else we need to link

--- fcpinyin/task.sh
+++ fcpinyin/task.sh
@@ -7,7 +7,7 @@ fi
 
 # If we specify cdylib or staticlib in Cargo.toml, the main.rs can't properly reference contents in lib.rs.
 if [ $1 == "gen-lib" ]; then
-    cargo rustc --lib --release --crate-type staticlib
+    cargo -Z unstable-options rustc --lib --release --crate-type staticlib -- --print native-static-libs
     exit 0
 fi
 
