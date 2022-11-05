374358eee5023f74f930be0150a90588ed0f3715
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-05 12:38:15 -0400

    Switch to using stable rust because analyzer failed to provide advanced check when project has multiple bin targets

--- fcpinyin/task.sh
+++ fcpinyin/task.sh
@@ -7,7 +7,7 @@ fi
 
 # If we specify cdylib or staticlib in Cargo.toml, the main.rs can't properly reference contents in lib.rs.
 if [ $1 == "gen-lib" ]; then
-    cargo -Z unstable-options rustc --lib --release --crate-type staticlib -- --print native-static-libs
+    cargo rustc --lib --release --crate-type staticlib
     exit 0
 fi
 
