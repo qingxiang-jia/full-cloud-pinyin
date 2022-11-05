e9d717db8cf2cc9ebb066fce16f737703796613c
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-05 12:37:34 -0400

    Add the Rust replacement of the task.sh

--- fcpinyin/src/cli/main.rs
+++ fcpinyin/src/cli/main.rs
@@ -1,19 +1,46 @@
 use regex::Regex;
-use std::fs;
 use std::env;
+use std::fs;
+use std::process::Command;
+use std::str;
 
 fn main() {
     // Read line from file
     let args: Vec<String> = env::args().collect();
-    let src_path = &args[1]; // 0 is the program itself
-    
-    cxx_gen_cc_to_separate_h(src_path);
+
+    if args.len() == 2 {
+        println!("No argument passed in");
+        return;
+    }
+
+    let arg2 = &args[2]; // 0 is cargo; 1 is the program itself; 2 is arg
+
+    match arg2.as_str() {
+        "gen-cxx" => {
+            let cxx_output = Command::new("cxxbridge")
+                .args(["src/core/ffi.rs"])
+                .output()
+                .expect("Faild to run cxxbridge");
+            let code = std::str::from_utf8(&cxx_output.stdout)
+                .expect("CXX output cannot be decoded as UTF-8.")
+                .to_owned();
+            cxx_gen_cc_to_separate_h(code);
+        }
+        "gen-lib" => {
+            let mut handle = Command::new("cargo")
+                .args(["rustc", "--lib", "--release", "--crate-type", "staticlib"])
+                .spawn()
+                .expect("ls command failed to start");
+            let _ = handle.wait();
+        }
+        &_ => {
+            println!("No such option.");
+            return;
+        }
+    }
 }
 
-fn cxx_gen_cc_to_separate_h(src_path: &String) {
-    // Read line from file
-    let code = fs::read_to_string(src_path).expect("Failed to load the file.");
-    
+fn cxx_gen_cc_to_separate_h(code: String) {
     // Set up regex to extract code
     let re_cxx = Regex::new("(namespace rust \\{(?:.|\n)+?\\} // namespace\n\\} // namespace cxxbridge1\n\\} // namespace rust)").expect("re_cxx is invalid.");
     let re_fcp = Regex::new("namespace fcp \\{(?:.|\n)+").expect("re_fcp is invalid.");
@@ -69,9 +96,9 @@ fn cxx_gen_cc_to_separate_h(src_path: &String) {
         .iter()
         .filter(|directive| directive.contains("\""))
         .collect();
-    
+
     // Assemble output code
-    
+
     let mut final_cxx = "".to_owned();
     final_cxx.push_str("#pragma once\n");
     for directive in &include_base {
