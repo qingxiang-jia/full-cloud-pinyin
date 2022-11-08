c5d6bfbddf2a34c26ea62567b9241da2c30a1caa
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-07 20:52:50 -0500

    Bring back bash, it exists for a reason

---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+if [ $# == 0 ]; then 
+    echo "No argument passed in"
+    exit 0
+fi
+
+# If we specify cdylib or staticlib in Cargo.toml, the main.rs can't properly reference contents in lib.rs.
+if [ $1 == "gen-lib" ]; then
+    cd ./fcpinyin
+    cargo rustc --lib --release --crate-type staticlib
+    cd ../
+    exit 0
+fi
+
+if [ $1 == "gen-cxx" ]; then
+    cd ./fcpinyin
+    cxxbridge src/core/ffi.rs > ffi.h
+    cd ../
+    exit 0
+fi
+
+if [ $1 == "init" ]; then
+    cd fcitx5
+    rm -rf ./build
+    mkdir build
+    cd ./build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+    cd ../../
+    exit 0
+fi
+
+if [ $1 == "build" ]; then
+    cd ./fcitx5/build
+    cmake --build .
+    cd ../../
+    exit 0
+fi
+
+if [ $1 == "install" ]; then
+    cd ./fcitx5/build
+    sudo cmake --install .
+    cd ../../
+    exit 0
+fi
+
+if [ $1 == "uninstall" ]; then
+    cd ./fcitx5/build
+    sudo xargs -I{} rm {} < ./install_manifest.txt
+    cd ../../
+    exit 0
+fi
+
+echo "Nothing done"
