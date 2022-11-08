8055ff2d14be14e1d21a36783c0604b807dc27a4
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-08 07:41:00 -0500

    Learn C based classic FFI

---
+++ c-based-ffi/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn r_add(a: u32, b: u32) -> u32 {
+    a + b
+}
---
+++ c-based-ffi/src/main.c
@@ -0,0 +1,10 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+extern uint32_t r_add(uint32_t, uint32_t);
+
+int main(void) {
+  uint32_t sum = r_add(1, 2);
+  printf("%" PRIu32 "\n", sum);
+}
