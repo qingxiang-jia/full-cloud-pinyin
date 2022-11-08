9ef0bdbc4c585c23f8dc64c4bb829b3027f37eaa
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-08 08:23:34 -0500

    Split definitions of Rust's function into a header

--- c-based-ffi/src/main.c
+++ c-based-ffi/src/main.c
@@ -1,8 +1,7 @@
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
-
-extern uint32_t r_add(uint32_t, uint32_t);
+#include "rust.h"
 
 int main(void) {
   uint32_t sum = r_add(1, 2);
---
+++ c-based-ffi/src/rust.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <inttypes.h>
+
+extern uint32_t r_add(uint32_t, uint32_t);
