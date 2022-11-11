8e502059521c24b16e58474e1e88e521ef56c4ce
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-11 08:03:44 -0500

    Add C side API for FFI experiment

--- c-based-ffi/src/c_api.h
+++ c-based-ffi/src/c_api.h
@@ -1,5 +1,54 @@
 #pragma once
 #include <inttypes.h>
 #include <stdint.h>
+#include <stdio.h>
+
+enum Key {
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z,
+  CTRL,
+  SHIFT,
+  ESC,
+  SPACE,
+  ENTER,
+  BACKSPACE
+};
 
 void c_print_u32(uint32_t);
+
+void key(enum Key key);
+
+void commit(uint8_t idx);
+
+void pageUp();
+
+void pageDown();
+
+// preedit should be a readonly pointer, it will be freed by Rust side
+// the same is true for candidates and lens
+// candidates points to an array of UTF-8 encoded CString
+void setState(char *preedit, int8_t **candidates, uint8_t *lens, uint16_t cnt);
--- c-based-ffi/src/main.c
+++ c-based-ffi/src/main.c
@@ -1,10 +1,33 @@
+#include "c_api.h"
+#include "rust_api.h"
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "rust_api.h"
-#include "c_api.h"
 
 int main(void) {
   uint32_t sum = r_add_cb(12, 10, c_print_u32);
   printf("The sum is %d, end of main.\n", sum);
+
+  key(SPACE);
+
+  commit(3);
+
+  pageUp();
+
+  pageDown();
+
+  char *preedit = "abc";
+
+  int8_t *candidates[4] = {
+      (int8_t[]){228, 184, 128}, // 一
+      (int8_t[]){233, 148, 174}, // 键
+      (int8_t[]){228, 184, 137}, // 三 
+      (int8_t[]){232, 191, 158}, // 连
+  };
+
+  uint8_t lens[] = {1, 2, 3, 4};
+
+  uint16_t cnt = 4;
+
+  setState(preedit, candidates, lens, cnt);
 }
