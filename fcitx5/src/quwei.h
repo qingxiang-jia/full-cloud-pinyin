3ff4280e721af9816af0f6f2623a2f6189c8c705
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-14 20:58:19 -0500

    Format using WebKit style

--- fcitx5/src/quwei.h
+++ fcitx5/src/quwei.h
@@ -8,6 +8,7 @@
 #define _FCITX5_QUWEI_QUWEI_H_
 
 #include "rustpinyin.h"
+#include <fcitx-utils/eventdispatcher.h>
 #include <fcitx-utils/inputbuffer.h>
 #include <fcitx/addonfactory.h>
 #include <fcitx/addonmanager.h>
@@ -16,7 +17,6 @@
 #include <fcitx/inputmethodengine.h>
 #include <fcitx/inputpanel.h>
 #include <fcitx/instance.h>
-#include <fcitx-utils/eventdispatcher.h>
 #include <memory>
 #include <vector>
 
@@ -24,12 +24,10 @@ class QuweiEngine;
 
 class QuweiEngine : public fcitx::InputMethodEngineV2 {
 public:
-    QuweiEngine(fcitx::Instance *instance);
+    QuweiEngine(fcitx::Instance* instance);
 
-    void activate(const fcitx::InputMethodEntry &entry,
-                  fcitx::InputContextEvent &event) override;
-    void keyEvent(const fcitx::InputMethodEntry &entry,
-                  fcitx::KeyEvent &keyEvent) override;
+    void activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& event) override;
+    void keyEvent(const fcitx::InputMethodEntry& entry, fcitx::KeyEvent& keyEvent) override;
 
     void updateUI();
     void getCandidatesAndUpdateAsync(bool append = false);
@@ -37,23 +35,23 @@ public:
     void preeditRemoveFront(int lenToRemove);
     void reset();
 
-    void reset(const fcitx::InputMethodEntry &,
-               fcitx::InputContextEvent &event) override;
+    void reset(const fcitx::InputMethodEntry&, fcitx::InputContextEvent& event) override;
 
     auto instance() const { return instance_; }
 
     std::unique_ptr<RustPinyin> rustPinyin_;
     std::unique_ptr<fcitx::EventDispatcher> dispatcher;
+
 private:
-    fcitx::Instance *instance_;
-    fcitx::InputContext *ic_;
-    fcitx::InputBuffer buffer_{{fcitx::InputBufferOption::AsciiOnly,
-                                fcitx::InputBufferOption::FixedCursor}};
+    fcitx::Instance* instance_;
+    fcitx::InputContext* ic_;
+    fcitx::InputBuffer buffer_ { { fcitx::InputBufferOption::AsciiOnly, fcitx::InputBufferOption::FixedCursor } };
     void getUpdateCandidatesRefreshUI(bool append);
 };
 
 class QuweiEngineFactory : public fcitx::AddonFactory {
-    fcitx::AddonInstance *create(fcitx::AddonManager *manager) override {
+    fcitx::AddonInstance* create(fcitx::AddonManager* manager) override
+    {
         FCITX_UNUSED(manager);
         return new QuweiEngine(manager->instance());
     }
