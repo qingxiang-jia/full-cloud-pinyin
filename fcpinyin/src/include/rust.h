0e35d64496665b6df63ce52b374132e85e9785f4
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-24 07:50:20 -0500

    Add callback types and their invocation

--- fcpinyin/src/include/rust.h
+++ fcpinyin/src/include/rust.h
@@ -1,5 +1,22 @@
-#include <stdio.h>
-#include <stdint.h>
+#include <cstdint>
 #include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef void (*FnCommit)(uint16_t);
+
+typedef void (*FnVoid)();
+
+typedef void (*FnSetCandidates)(int16_t **candidates, size_t cnt);
+
+typedef void (*FnSetPreedit)(char *preedit);
 
 extern "C" void on_key_press(uint16_t);
+
+extern "C" void register_fn_commit(FnCommit callback);
+
+extern "C" void register_fn_void(FnVoid callback);
+
+extern "C" void register_fn_set_candidates(FnSetCandidates callback);
+
+extern "C" void register_fn_set_preedit(FnSetPreedit callback);
