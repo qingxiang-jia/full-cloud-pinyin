2273252ce661f0f3f9baaba6ec131030ca565a9a
Qingxiang Jia <ybjqx3340@gmail.com>
2022-11-25 08:34:20 -0500

    Call from Rust with array of cstring (UTF-8) works

--- fcitx5/src/quwei.cpp
+++ fcitx5/src/quwei.cpp
@@ -75,6 +75,7 @@ QuweiEngine::QuweiEngine(fcitx::Instance* instance)
     register_fn_void(page_up);
     register_fn_commit(commit);
     register_fn_set_preedit(set_preedit);
+    register_fn_set_candidates(set_candidates);
 }
 
 void QuweiEngine::activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& event)
