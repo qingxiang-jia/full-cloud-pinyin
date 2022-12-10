cadebe17db13e1beee81f73468de2107d3d8ec36
Qingxiang Jia <ybjqx3340@gmail.com>
2022-12-10 12:42:22 -0500

    Remove unnecessary comments

--- fcpinyin/src/include/rust.h
+++ fcpinyin/src/include/rust.h
@@ -18,16 +18,12 @@ typedef struct FcpOpaque {
 } FcpOpaque;
 
 extern "C" const FcpOpaque* new_fcp();
-
-/* ↓ CALLBACK REGISTRATION */
 extern "C" void register_callbacks(
     FcpOpaque *opaque, FnVoid set_loading, FnSetCandidates set_candidates,
     FnSetCandidates append_candidates, FnVoid clear_candidates,
     FnSetPreedit set_preedit, FnCanPageUp can_page_up, FnVoid page_up,
     FnVoid page_down, FnVoid prev, FnVoid next, FnCommit commit,
     FnSetPreedit commit_preedit, FnVoid commit_candidate_by_fixed_key);
-/* ↑ CALLBACK REGISTRATION */
-
 extern "C" bool on_key_press(uint16_t);
 /* ↑ C++ -> Rust */
 
