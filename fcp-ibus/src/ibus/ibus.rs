2dff72e4f7a2a3cad502d032b1f5552936cf5670
Qingxiang Jia <ybjqx3340@gmail.com>
2023-05-20 12:23:15 -0400

    See if it works so far, no

--- fcp-ibus/src/ibus/ibus.rs
+++ fcp-ibus/src/ibus/ibus.rs
@@ -1,49 +1,28 @@
 use zbus::Connection;
 
-// DBus addresses
-static WELL_KNOWN_IBUS_ADDRESS: &str = "/org/freedesktop/IBus";
-static WELL_KNOWN_IBUS_FACTORY_ADDRESS: &str = "/org/freedesktop/IBus/Factory";
-static WELL_KNOWN_DBUS_ADDRESS: &str = "/org/freedesktop/DBus";
+use super::proxy::ibus::IBusProxy;
 
 // DBus interfaces
-static IBUS_INTERFACE: &str = "org.freedesktop.IBus";
-static IBUS_FACTORY_INTERFACE: &str = "org.freedesktop.IBus.Factory";
-static IBUS_ENGINE_INTERFACE: &str = "org.freedesktop.IBus.Engine";
-static IBUS_SERVICE_INTERFACE: &str = "org.freedesktop.IBus.Service";
-static IBUS_PROPERTIES_INTERFACE: &str = "org.freedesktop.DBus.Properties";
-static DBUS_INTERFACE: &str = "org.freedesktop.DBus";
-
-pub struct IBus {
-    ibus_address: String,
-    dbus_address: String,
-    ibus_factory_address: String,
-    ibus_interface: String,
-    dbus_interface: String,
-    ibus_engine_interface: String,
-    ibus_service_interface: String,
-    ibus_properties_interface: String,
-}
+pub struct IBus {}
 
 impl IBus {
     pub fn new() -> Self {
-        IBus {
-            ibus_address: WELL_KNOWN_IBUS_ADDRESS.to_string(),
-            dbus_address: WELL_KNOWN_DBUS_ADDRESS.to_string(),
-
-            ibus_factory_address: WELL_KNOWN_IBUS_FACTORY_ADDRESS.to_string(),
-
-            ibus_interface: IBUS_INTERFACE.to_string(),
-            dbus_interface: DBUS_INTERFACE.to_string(),
-
-            ibus_engine_interface: IBUS_ENGINE_INTERFACE.to_string(),
-            ibus_service_interface: IBUS_SERVICE_INTERFACE.to_string(),
-            ibus_properties_interface: IBUS_PROPERTIES_INTERFACE.to_string(),
-        }
+        IBus {}
     }
 
-    pub async fn init() {
+    pub async fn init(&self) {
         // client init
-        let conn_to_ibus = Connection::session().await.expect("Failed to get a DBus session connection.");
+        let conn_to_ibus = Connection::session()
+            .await
+            .expect("Failed to get a DBus session connection.");
+        let proxy_ibus = IBusProxy::new(&conn_to_ibus)
+            .await
+            .expect("Failed to get a connection to IBus.");
+        let engines = proxy_ibus
+            .engines()
+            .await
+            .expect("Failed to get IBus engines.");
+        println!("Number of engines on IBus: {}", engines.len());
         // server object init
     }
 }
--- fcp-ibus/src/main.rs
+++ fcp-ibus/src/main.rs
@@ -1,6 +1,10 @@
 #![feature(fmt_helpers_for_derive)]
+
+use ibus::ibus::IBus;
 mod ibus;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let ibus = IBus::new();
+    ibus.init().await;
 }
