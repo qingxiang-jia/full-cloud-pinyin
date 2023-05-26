5ffc2eb853cef40280fc5a0dd1d69175890b22dd
Qingxiang Jia <ybjqx3340@gmail.com>
2023-05-26 08:20:10 -0400

    Add reporting code

--- fcp-ibus/src/ibus/fcp_engine.rs
+++ fcp-ibus/src/ibus/fcp_engine.rs
@@ -6,81 +6,107 @@ pub struct FcpEngine {}
 
 impl IBusEngine for FcpEngine {
     fn process_key_event(&mut self, keyval: u32, keycode: u32, state: u32) -> Result<bool, dbus::MethodErr> {
-        unimplemented!();
+        println!("process_key_event: keyval={keyval} keycode={keycode} state={state}");
+        Ok(true)
     }
     fn set_cursor_location(&mut self, x_: i32, y_: i32, w_: i32, h_: i32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("set_cursor_location: x={x_} y={y_} w={w_} h={h_}");
+        Ok(())
     }
     fn process_hand_writing_event(&mut self, coordinates: Vec<f64>) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("process_hand_writing_event");
+        Ok(())
     }
     fn cancel_hand_writing(&mut self, n_strokes: u32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("cancel_hand_writing");
+        Ok(())
     }
     fn set_capabilities(&mut self, caps: u32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("set_capabilities: caps={caps}");
+        Ok(())
     }
     fn property_activate(&mut self, name: String, state: u32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("property_activate");
+        Ok(())
     }
     fn property_show(&mut self, name: String) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("property_show: name={name}");
+        Ok(())
     }
     fn property_hide(&mut self, name: String) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("property_hide: name={name}");
+        Ok(())
     }
     fn candidate_clicked(&mut self, index: u32, button: u32, state: u32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("candidate_clicked: index={index} button={button} state={state}");
+        Ok(())
     }
     fn focus_in(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("focus_in");
+        Ok(())
     }
     fn focus_in_id(&mut self, object_path: String, client: String) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("focus_in_id: object_path={object_path} client={client}");
+        Ok(())
     }
     fn focus_out(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("focus_out");
+        Ok(())
     }
     fn focus_out_id(&mut self, object_path: String) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("focus_out_id: object_path={object_path}");
+        Ok(())
     }
     fn reset(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("reset");
+        Ok(())
     }
     fn enable(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("enable");
+        Ok(())
     }
     fn disable(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("disable");
+        Ok(())
     }
     fn page_up(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("page_up");
+        Ok(())
     }
     fn page_down(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("page_down");
+        Ok(())
     }
     fn cursor_up(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("cursor_up");
+        Ok(())
     }
     fn cursor_down(&mut self) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("cursor_down");
+        Ok(())
     }
     fn set_surrounding_text(&mut self, text: arg::Variant<Box<dyn arg::RefArg + 'static>>, cursor_pos: u32, anchor_pos: u32) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("set_surrounding_text: cursor_pos={cursor_pos}");
+        Ok(())
     }
     fn panel_extension_received(&mut self, event: arg::Variant<Box<dyn arg::RefArg + 'static>>) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("panel_extension_received");
+        Ok(())
     }
     fn panel_extension_register_keys(&mut self, data: arg::Variant<Box<dyn arg::RefArg + 'static>>) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("panel_extension_register_keys");
+        Ok(())
     }
     fn set_content_type(&self, value: (u32, u32)) -> Result<(), dbus::MethodErr> {
-        unimplemented!();
+        println!("set_content_type");
+        Ok(())
     }
     fn focus_id(&self) -> Result<(bool), dbus::MethodErr> {
-        unimplemented!();
+        println!("focus_id");
+        Ok(true)
     }
     fn active_surrounding_text(&self) -> Result<(bool), dbus::MethodErr> {
-        unimplemented!();
+        println!("active_surrounding_text");
+        Ok(true)
     }
 }
