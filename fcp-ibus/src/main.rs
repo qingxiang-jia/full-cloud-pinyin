7cc4697c5df5bd2e2798d0a369f527c02d3f5266
Qingxiang Jia <ybjqx3340@gmail.com>
2023-05-26 21:24:38 -0400

    Set up DBus sever

--- fcp-ibus/src/main.rs
+++ fcp-ibus/src/main.rs
@@ -1,9 +1,14 @@
 #![feature(fmt_helpers_for_derive)]
 
-use ibus::client_factory::new_ibus_client;
+use ibus::{client_factory::new_ibus_client, dbus_server::engine::{register_org_freedesktop_ibus_engine, IBusEngine}, fcp_engine::FcpEngine};
+use dbus_crossroads::{self, Crossroads, IfaceToken};
+use ibus::fcp_engine;
 mod ibus;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let ibus = new_ibus_client();
+    let mut cr = Crossroads::new();
+    let engine = FcpEngine {};
+    let token: IfaceToken<FcpEngine> = register_org_freedesktop_ibus_engine(&mut cr);
+    cr.insert("/", &[token], engine);
 }
