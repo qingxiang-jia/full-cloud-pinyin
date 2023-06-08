15a481a2b1a6ffac68fadb4bdb8a3ec40f719e9f
Qingxiang Jia <ybjqx3340@gmail.com>
2023-06-08 07:46:38 -0400

    Add debugging statements

--- fcp-zbus/src/engine.rs
+++ fcp-zbus/src/engine.rs
@@ -8,10 +8,12 @@ pub struct Fcp {
 #[dbus_interface(name = "org.freedesktop.IBus.FcPinyin")]
 impl Fcp {
     pub fn create_engine(&self, name: &str) -> ObjectPath {
+        println!("create_engine called by IBus.");
         ObjectPath::from_str_unchecked("/org/freedesktop/IBus/Engine/FcPinyin")
     }
 
     pub fn process_key_event(&self, keyval: u32, keycode: u32, state: u32) -> bool {
+        println!("process_key_event called by IBus.");
         return true
     }
 }
