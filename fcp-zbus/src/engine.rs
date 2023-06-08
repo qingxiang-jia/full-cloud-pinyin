035954b23e140698c872d80a9a45614949468a42
Qingxiang Jia <ybjqx3340@gmail.com>
2023-06-07 21:55:24 -0400

    Add more function

--- fcp-zbus/src/engine.rs
+++ fcp-zbus/src/engine.rs
@@ -8,4 +8,8 @@ impl Fcp {
     pub fn create_engine(&self, name: &str) -> ObjectPath {
         ObjectPath::from_str_unchecked("/org/freedesktop/IBus/Engine/FcPinyin")
     }
+
+    pub fn process_key_event(&self, keyval: u32, keycode: u32, state: u32) -> bool {
+        return true
+    }
 }
