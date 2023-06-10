aef60d3ff021cb7159f219e7397fd33d539eb6e4
Qingxiang Jia <ybjqx3340@gmail.com>
2023-06-10 10:18:16 -0400

    Don't return true since we didn't handle

--- fcp-zbus/src/engine.rs
+++ fcp-zbus/src/engine.rs
@@ -25,7 +25,7 @@ impl FcpFactory {
 impl FcpEngine {
     pub fn process_key_event(&self, keyval: u32, keycode: u32, state: u32) -> bool {
         println!("process_key_event called by IBus.");
-        return true
+        return false;
     }
 }
 
