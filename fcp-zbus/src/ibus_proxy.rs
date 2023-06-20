2ce7bdda2925f8c9d6124b63fd1546c465c95dd0
Qingxiang Jia <ybjqx3340@gmail.com>
2023-06-20 08:14:14 -0400

    Implement signals for IBus Engine interface

--- fcp-zbus/src/ibus_proxy.rs
+++ fcp-zbus/src/ibus_proxy.rs
@@ -1,24 +1,64 @@
 use zbus::Connection;
+use zvariant::Value;
 
 static ENGINE_PATH: &str = "/org/freedesktop/IBus/Engine/FcPinyin";
 static ENGINE_IFACE: &str = "org.freedesktop.IBus.Engine";
 
-pub async fn commit_text(conn: &Connection, text: &zbus::zvariant::Value<'_>) {
+pub async fn commit_text(conn: &Connection, text: &Value<'_>) {
+    conn.emit_signal(None::<&str>, ENGINE_PATH, ENGINE_IFACE, "CommitText", text)
+        .await
+        .expect("Failed to emit CommitText signal.");
+}
+
+pub async fn update_preedit_text(
+    conn: &Connection,
+    text: &Value<'_>,
+    cursor_pos: u32,
+    visible: bool,
+) {
     conn.emit_signal(
         None::<&str>,
         ENGINE_PATH,
         ENGINE_IFACE,
-        "CommitText",
-        text,
+        "UpdatePreeditText",
+        &(text, cursor_pos, visible, 0),
     )
     .await
-    .expect("Failed to emit CommitText signal.");
+    .expect("Failed to emit UpdatePreeditText signal.");
 }
 
-// UpdatePreeditText: text: IBusText, cursor_pos u32, visible bool, 0
+pub async fn update_lookup_table(conn: &Connection, lt: &Value<'_>, visible: bool) {
+    conn.emit_signal(
+        None::<&str>,
+        ENGINE_PATH,
+        ENGINE_IFACE,
+        "UpdateLookupTable",
+        &(lt, visible),
+    )
+    .await
+    .expect("Failed to emit UpdateLookupTable signal.");
+}
 
-// UpdateLookupTable: lt: IBusLookupTable, visible bool
+pub async fn show_lookup_table(conn: &Connection) {
+    conn.emit_signal(
+        None::<&str>,
+        ENGINE_PATH,
+        ENGINE_IFACE,
+        "ShowLookupTable",
+        &(),
+    )
+    .await
+    .expect("Failed to emit ShowLookupTable signal.");
+}
 
-// ShowLookupTable
-
-// HideLookupTable
+pub async fn hide_lookup_table(conn: &Connection) {
+    conn.emit_signal(
+        None::<&str>,
+        ENGINE_PATH,
+        ENGINE_IFACE,
+        "HideLookupTable",
+        &(),
+    )
+    .await
+    .expect("Failed to emit HideLookupTable signal.");
+}
