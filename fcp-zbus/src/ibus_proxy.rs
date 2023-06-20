76866ec0fc8be44f2b54e14df59e029178980b3f
Qingxiang Jia <ybjqx3340@gmail.com>
2023-06-20 08:06:41 -0400

    Since they will be the same, extract

--- fcp-zbus/src/ibus_proxy.rs
+++ fcp-zbus/src/ibus_proxy.rs
@@ -1,13 +1,24 @@
 use zbus::Connection;
 
+static ENGINE_PATH: &str = "/org/freedesktop/IBus/Engine/FcPinyin";
+static ENGINE_IFACE: &str = "org.freedesktop.IBus.Engine";
+
 pub async fn commit_text(conn: &Connection, text: &zbus::zvariant::Value<'_>) {
     conn.emit_signal(
         None::<&str>,
-        "/org/freedesktop/IBus/Engine/FcPinyin",
-        "org.freedesktop.IBus.Engine",
+        ENGINE_PATH,
+        ENGINE_IFACE,
         "CommitText",
         text,
     )
     .await
     .expect("Failed to emit CommitText signal.");
 }
+
+// UpdatePreeditText: text: IBusText, cursor_pos u32, visible bool, 0
+
+// UpdateLookupTable: lt: IBusLookupTable, visible bool
+
+// ShowLookupTable
+
+// HideLookupTable
