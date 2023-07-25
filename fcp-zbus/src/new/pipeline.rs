e5d9768d36594c595f00dab733e392ecb6509164
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-25 08:02:35 -0400

    Make pipeline segments more clear

--- fcp-zbus/src/new/pipeline.rs
+++ fcp-zbus/src/new/pipeline.rs
@@ -6,19 +6,24 @@ pub struct Pipeline {
 
 impl Pipeline {
     pub async fn accept(&self, keyval: u32, keycode: u32, state: u32) -> bool {
+        /* BEGIN mode switching */
         let output = self
             .mode_switcher
             .process_key_event(keyval, keycode, state)
             .await;
-        
+
         let has_handled = output.get_done_data();
         if has_handled.is_some() {
             return has_handled.expect("has_handled should have value but doesn't.");
         }
-        
+
         let (key, should_reset) = output
             .get_continue_data()
             .expect("ModeSwitcherReturn is Continue but doesn't have data.");
+        /* END mode switching */
+
+        /* BEGIN session switching */
+        /* END session switching */
 
         return false;
     }
