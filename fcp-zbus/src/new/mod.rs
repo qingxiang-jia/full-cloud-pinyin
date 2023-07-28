c854b72c88bfd9cfbb53090aaba0c76e826a0681
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-28 08:24:09 -0400

    Handlers are probably not useful

--- fcp-zbus/src/new/mod.rs
+++ fcp-zbus/src/new/mod.rs
@@ -1,7 +1,4 @@
 mod pipeline;
 mod mode_switcher;
 mod dispatcher;
-mod pinyin_handler;
 mod cloud_pinyin_client;
-mod number_handler;
-mod symbol_handler;
--- fcp-zbus/src/new/pinyin_handler.rs
+++
@@ -1,7 +0,0 @@
-use std::sync::{Mutex, Arc};
-
-use super::cloud_pinyin_client::CloudPinyinClient;
-
-pub struct PinyinHandler {
-    client: Arc<Mutex<CloudPinyinClient>>
-}
