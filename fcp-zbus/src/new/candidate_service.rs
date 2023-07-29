645368de290c0b648c487ad8740f0f75dd3e573d
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-28 20:16:08 -0400

    Put state in candidate service

--- fcp-zbus/src/new/candidate_service.rs
+++ fcp-zbus/src/new/candidate_service.rs
@@ -1,14 +1,10 @@
+use std::sync::Mutex;
+
 use zbus::Connection;
 
 use crate::ibus_proxy::IBusProxy;
 
 use super::candidate::Candidate;
-
-pub struct CandidateService {
-    lt_size: usize,
-    levels: Vec<usize>,
-    ibus: IBusProxy,
-}
 
 struct State {
     session: bool,
@@ -26,11 +22,21 @@ impl State {
     }
 }
 
+unsafe impl Sync for State {} // State is safe to share between threads
+
+pub struct CandidateService {
+    lt_size: usize,
+    levels: Vec<usize>,
+    state: Mutex<State>,
+    ibus: IBusProxy,
+}
+
 impl CandidateService {
     pub fn new(conn: &Connection) -> CandidateService {
         CandidateService {
             lt_size: 5,
             levels: vec![11, 21, 41, 81, 161, 321, 641, 1281],
+            state: Mutex::new(State::new()),
             ibus: IBusProxy::new(&conn),
         }
     }
