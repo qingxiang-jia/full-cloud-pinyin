8ef12f8d1b6777048ba537d8b74da040d7d7e5fd
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-28 20:10:13 -0400

    Add new()

--- fcp-zbus/src/new/candidate_service.rs
+++ fcp-zbus/src/new/candidate_service.rs
@@ -1,3 +1,5 @@
+use zbus::Connection;
+
 use crate::ibus_proxy::IBusProxy;
 
 pub struct CandidateService {
@@ -7,23 +9,21 @@ pub struct CandidateService {
 }
 
 impl CandidateService {
-    pub fn page_up() {
-
+    pub fn new(conn: &Connection) -> CandidateService {
+        CandidateService {
+            lt_size: 5,
+            levels: vec![11, 21, 41, 81, 161, 321, 641, 1281],
+            ibus: IBusProxy::new(&conn),
+        }
     }
 
-    pub fn page_down() {
+    pub fn page_up() {}
 
-    }
+    pub fn page_down() {}
 
-    pub fn set_candidates() {
+    pub fn set_candidates() {}
 
-    }
+    pub fn select() {}
 
-    pub fn select() {
-
-    }
-
-    pub fn clear() {
-        
-    }
+    pub fn clear() {}
 }
