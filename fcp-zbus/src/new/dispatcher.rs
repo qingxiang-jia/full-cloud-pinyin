4e6958597b5e522ee0680c2f49b600d684888a94
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-29 22:13:58 -0400

    WIP dispatcher

--- fcp-zbus/src/new/dispatcher.rs
+++ fcp-zbus/src/new/dispatcher.rs
@@ -1,3 +1,41 @@
+use std::sync::Mutex;
+
+use zbus::Connection;
+
+use crate::keys::Key;
+
+use super::candidate_service::CandidateService;
+
+
+struct State {
+    session: bool,
+    preedit: Vec<char>,
+}
+
+impl State {
+    pub fn new() -> State {
+        State {
+            session: false,
+            preedit: Vec::new(),
+        }
+    }
+}
+
+unsafe impl Sync for State {} // State is safe to share between threads
 
 pub struct Dispatcher {
+    state: Mutex<State>,
+    cs: CandidateService,
+}
+
+impl Dispatcher {
+    pub fn new(conn: &Connection) -> Dispatcher {
+        Dispatcher {
+            state: Mutex::new(State::new()),
+            cs: CandidateService::new(conn),
+        }
+    }
+    
+    pub async fn on_input(&self, key: Key, should_reset: bool) {
+    }
 }
