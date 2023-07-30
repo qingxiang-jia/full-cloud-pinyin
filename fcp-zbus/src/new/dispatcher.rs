a0013a6c2ea2ed6c333427ec2eee4eafc5fff756
Qingxiang Jia <ybjqx3340@gmail.com>
2023-07-30 09:16:52 -0400

    Add placeholder functions and use candidate service

--- fcp-zbus/src/new/dispatcher.rs
+++ fcp-zbus/src/new/dispatcher.rs
@@ -4,7 +4,7 @@ use zbus::Connection;
 
 use crate::keys::Key;
 
-use super::candidate_service::CandidateService;
+use super::{candidate_service::CandidateService, cloud_pinyin_client::CloudPinyinClient};
 
 
 struct State {
@@ -24,6 +24,7 @@ unsafe impl Sync for State {} // State is safe to share between threads
 pub struct Dispatcher {
     state: Mutex<State>,
     cs: CandidateService,
+    client: CloudPinyinClient,
 }
 
 impl Dispatcher {
@@ -31,9 +32,32 @@ impl Dispatcher {
         Dispatcher {
             state: Mutex::new(State::new()),
             cs: CandidateService::new(conn),
+            client: CloudPinyinClient::new(),
         }
     }
     
-    pub async fn on_input(&self, key: Key, should_reset: bool) {
+    pub async fn on_input(&self, key: Key, should_reset: bool) -> bool {
+        if should_reset {
+            self.cs.clear().await;
+            return false;
+        }
+
+        !unimplemented!()
+    }
+
+    pub fn handle_pinyin(key: Key) -> bool {
+        !unimplemented!()
+    }
+
+    pub fn handle_select(key: Key) -> bool {
+        !unimplemented!()
+    }
+
+    pub fn handle_symbol(key: Key) -> bool {
+        !unimplemented!()
+    }
+
+    pub fn handle_control(key: Key) -> bool {
+        !unimplemented!()
     }
 }
