36532cdade4730d143e1fdbe6624ee9e7f9c3a36
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-15 21:43:18 -0500

    Add receive and send sides

--- fcp-pmq/src/main.rs
+++ fcp-pmq/src/main.rs
@@ -1,7 +1,44 @@
+use std::{env, io};
+
 use nix::mqueue::{mq_close, mq_open, mq_receive, mq_send, MQ_OFlag, MqdT};
 use nix::sys::stat::Mode;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    for arg in args {
+        println!("arg: {}", &arg);
+    }
+}
+
+fn run_send() {
+    let q = open_send("fcp_pmq_queue");
+    let mut input = String::new();
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input == "stop" {
+                    close(q);
+                    return;
+                }
+                send(&q, input.as_bytes());
+                println!("Sent {input}");
+            }
+            Err(error) => println!("error: {error}"),
+        }
+    }
+}
+
+fn run_recv() {
+    let q = open_recv("fcp_pmq_queue");
+    let mut buf = vec![0_u8; 1024];
+    loop {
+        let received = recv(&q, &mut buf);
+        let _ = match std::str::from_utf8(&buf[..received]) {
+            Ok(v) => println!("received: {}", v),
+            Err(e) => println!("Decoding UTF-8 string failed: {}", e),
+        };
+    }
+}
 
 fn open_send(name: &str) -> MqdT {
     mq_open(
