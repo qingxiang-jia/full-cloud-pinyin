ac4fbae2227a4a5e8da8accc84abc409a8353b8f
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-18 09:23:40 -0500

    Named pipe doesn't quite work
    - The fifo is easy to make, but the File API doesn't support read and block if there's no new data in file.
    - It can work with other things but not with the vanilla File API.
    - Other languages may also not have friendly API for this use case.

--- fcp-fifo/src/main.rs
+++ fcp-fifo/src/main.rs
@@ -1,7 +1,8 @@
 use std::{
+    env,
     fs::File,
     io::{self, Read, Write},
-    path::Path, env,
+    path::Path, os::unix::fs::FileExt,
 };
 
 use nix::{sys::stat::Mode, unistd::mkfifo};
@@ -9,8 +10,7 @@ use nix::{sys::stat::Mode, unistd::mkfifo};
 fn main() {
     let path = "/home/lee/Downloads/my_fifo";
     make_fifo(path);
-    let mut pipe = File::open(path).expect("Faild to open the pipe.");
-    
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 || (args[1] != "send" && args[1] != "recv") {
         println!("Usage: parameter is either send or recv.");
@@ -18,9 +18,17 @@ fn main() {
     }
     let param = &args[1];
     if param == "send" {
+        let mut pipe = File::options()
+            .write(true)
+            .open(path)
+            .expect("Faild to open the pipe.");
         run_send(&mut pipe);
     }
     if param == "recv" {
+        let mut pipe = File::options()
+            .read(true)
+            .open(path)
+            .expect("Faild to open the pipe.");
         run_recv(&mut pipe);
     }
 }
@@ -43,7 +51,6 @@ fn run_send(pipe: &mut File) {
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
                 send(pipe, input.as_bytes());
-                println!("Sent");
             }
             Err(e) => println!("Failed to read from stdin: {}", e),
         }
@@ -58,6 +65,7 @@ fn send(pipe: &mut File, payload: &[u8]) {
 }
 
 fn run_recv(pipe: &mut File) {
+    println!("run_recv");
     let mut payload: Vec<u8> = Vec::new();
     loop {
         recv(pipe, &mut payload);
