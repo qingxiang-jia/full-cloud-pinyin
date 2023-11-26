cfdc095433a64528bcb2ab4239f876e4dc907aec
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-26 17:24:03 -0500

    Add factory to make an instance of IMS

--- fcp-zmq/ims/ims.h
+++ fcp-zmq/ims/ims.h
@@ -3,7 +3,9 @@
 #include <cstdint>
 #include <fcitx-utils/eventdispatcher.h>
 #include <fcitx-utils/inputbuffer.h>
+#include <fcitx-utils/macros.h>
 #include <fcitx/addonfactory.h>
+#include <fcitx/addoninstance.h>
 #include <fcitx/addonmanager.h>
 #include <fcitx/candidatelist.h>
 #include <fcitx/inputcontext.h>
@@ -16,11 +18,19 @@
 
 class ImsEngine;
 
-class ImsEngine : public fcitx::InputMethodEngineV4 {
+class ImsEngine : public fcitx::InputMethodEngineV2 {
 public:
     ImsEngine(fcitx::Instance* instance);
 
     void activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& event) override;
     void keyEvent(const fcitx::InputMethodEntry& entry, fcitx::KeyEvent& keyEvent) override;
     void reset(const fcitx::InputMethodEntry&, fcitx::InputContextEvent& event) override;
+};
+
+class ImsEngineFactory : public fcitx::AddonFactory {
+    fcitx::AddonInstance* create(fcitx::AddonManager* manager) override
+    {
+        FCITX_UNUSED(manager);
+        return new ImsEngine(manager->instance());
+    }
 };
