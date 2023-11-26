4cf0a2831edcf3c83eba42d9373df83767380cf6
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-26 17:31:32 -0500

    Add IMS noop implementations

---
+++ fcp-zmq/ims/ims.cpp
@@ -0,0 +1,49 @@
+#include "ims.h"
+#include <chrono>
+#include <cstddef>
+#include <fcitx-utils/eventdispatcher.h>
+#include <fcitx-utils/i18n.h>
+#include <fcitx-utils/key.h>
+#include <fcitx-utils/keysymgen.h>
+#include <fcitx-utils/log.h>
+#include <fcitx-utils/macros.h>
+#include <fcitx-utils/utf8.h>
+#include <fcitx/candidatelist.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/instance.h>
+#include <fcitx/text.h>
+#include <fcitx/userinterfacemanager.h>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <new>
+#include <thread>
+#include <utility>
+#include <vector>
+
+ImsEngine* engine;
+
+ImsEngine::ImsEngine(fcitx::Instance* instance)
+{
+    FCITX_UNUSED(instance);
+
+    engine = this;
+}
+
+void ImsEngine::activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& event)
+{
+    FCITX_UNUSED(entry);
+    FCITX_UNUSED(event);
+}
+
+void ImsEngine::keyEvent(const fcitx::InputMethodEntry& entry, fcitx::KeyEvent& keyEvent)
+{
+    FCITX_UNUSED(entry);
+    FCITX_UNUSED(keyEvent);
+}
+
+void ImsEngine::reset(const fcitx::InputMethodEntry&, fcitx::InputContextEvent& event)
+{
+    FCITX_UNUSED(event);
+}
