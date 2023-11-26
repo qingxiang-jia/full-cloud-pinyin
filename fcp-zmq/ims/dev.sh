6eca79290a1c6850bbf07c9c93e3b8a1967482b8
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-26 17:03:25 -0500

    Set up input method server project

---
+++ fcp-zmq/ims/dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $1 == "init_cmake" ]; then
+    mkdir -p build
+    cd ./build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug ..
+    cd ../
+    exit 0
+fi
+
+if [ $1 == "clean" ]; then
+    if [ -d ./build ]; then
+        rm -rf ./build
+    fi
+    exit 0
+fi
+
+echo "Nothing done"
---
+++ fcp-zmq/ims/ims.h
@@ -0,0 +1,16 @@
+// #pragma once
+
+// #include <cstdint>
+// #include <fcitx-utils/eventdispatcher.h>
+// #include <fcitx-utils/inputbuffer.h>
+// #include <fcitx/addonfactory.h>
+// #include <fcitx/addonmanager.h>
+// #include <fcitx/candidatelist.h>
+// #include <fcitx/inputcontext.h>
+// #include <fcitx/inputcontextproperty.h>
+// #include <fcitx/inputmethodengine.h>
+// #include <fcitx/inputpanel.h>
+// #include <fcitx/instance.h>
+// #include <memory>
+// #include <vector>
+
