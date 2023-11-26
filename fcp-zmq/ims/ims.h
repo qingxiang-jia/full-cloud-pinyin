2e006b31b22888e62ae0f35bf8bf03638cc323b7
Qingxiang Jia <ybjqx3340@gmail.com>
2023-11-26 17:19:36 -0500

    Add IMS header

--- fcp-zmq/ims/ims.h
+++ fcp-zmq/ims/ims.h
@@ -1,16 +1,26 @@
-// #pragma once
+#pragma once
 
-// #include <cstdint>
-// #include <fcitx-utils/eventdispatcher.h>
-// #include <fcitx-utils/inputbuffer.h>
-// #include <fcitx/addonfactory.h>
-// #include <fcitx/addonmanager.h>
-// #include <fcitx/candidatelist.h>
-// #include <fcitx/inputcontext.h>
-// #include <fcitx/inputcontextproperty.h>
-// #include <fcitx/inputmethodengine.h>
-// #include <fcitx/inputpanel.h>
-// #include <fcitx/instance.h>
-// #include <memory>
-// #include <vector>
+#include <cstdint>
+#include <fcitx-utils/eventdispatcher.h>
+#include <fcitx-utils/inputbuffer.h>
+#include <fcitx/addonfactory.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/candidatelist.h>
+#include <fcitx/inputcontext.h>
+#include <fcitx/inputcontextproperty.h>
+#include <fcitx/inputmethodengine.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/instance.h>
+#include <memory>
+#include <vector>
 
+class ImsEngine;
+
+class ImsEngine : public fcitx::InputMethodEngineV4 {
+public:
+    ImsEngine(fcitx::Instance* instance);
+
+    void activate(const fcitx::InputMethodEntry& entry, fcitx::InputContextEvent& event) override;
+    void keyEvent(const fcitx::InputMethodEntry& entry, fcitx::KeyEvent& keyEvent) override;
+    void reset(const fcitx::InputMethodEntry&, fcitx::InputContextEvent& event) override;
+};
