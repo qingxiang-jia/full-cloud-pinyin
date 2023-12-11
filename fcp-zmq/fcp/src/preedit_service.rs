8a26b8ac048432a8a1017f38c2f9ae049ef31d9f
Qingxiang Jia <ybjqx3340@gmail.com>
2023-12-10 22:28:38 -0500

    Implement preedit service

---
+++ fcp-zmq/fcp/src/preedit_service.rs
@@ -0,0 +1,69 @@
+use std::sync::Arc;
+
+use std::sync::Mutex;
+
+use crate::ims::Req;
+
+struct State {
+    preedit: Vec<char>,
+}
+
+impl State {
+    fn new() -> State {
+        State {
+            preedit: Vec::new(),
+        }
+    }
+}
+
+pub struct PreeditService {
+    zmq: Arc<Mutex<Req>>,
+    state: Mutex<State>,
+}
+
+impl PreeditService {
+    pub fn new(ibus: Arc<Mutex<Req>>) -> PreeditService {
+        PreeditService {
+            zmq: ibus,
+            state: Mutex::new(State::new()),
+        }
+    }
+
+    pub async fn push(&self, c: char) {
+        let mut state = self.state.lock().expect("push: Failed to lock state.");
+        state.preedit.push(c);
+        let preedit: String = state.preedit.iter().cloned().collect();
+
+        drop(state);
+
+        self.zmq
+            .lock()
+            .expect("push: Failed to lock zmq.")
+            .update_preedit(&preedit)
+    }
+
+    pub async fn pop(&self) -> Option<char> {
+        let mut state = self.state.lock().expect("pop: Failed to lock state.");
+        let popped = state.preedit.pop();
+        let preedit: String = state.preedit.iter().cloned().collect();
+
+        drop(state);
+
+        self.zmq
+            .lock()
+            .expect("pop: Failed to lock zmq.")
+            .update_preedit(&preedit);
+
+        popped
+    }
+
+    pub async fn to_string(&self) -> String {
+        let state = self.state.lock().expect("to_string: Failed to lock state.");
+        state.preedit.iter().cloned().collect()
+    }
+
+    pub async fn clear(&self) {
+        let mut state = self.state.lock().expect("clear: Failed to lock state.");
+        state.preedit.clear();
+    }
+}
