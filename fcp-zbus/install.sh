f93157dca0adbe95f7a626ff0740cd6f073e3366
Qingxiang Jia <ybjqx3340@gmail.com>
2023-12-26 12:49:37 -0500

    Move IBus implementation to its own repository

--- fcp-zbus/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo cp fcpinyin.xml /usr/share/ibus/component/
--- fcp-zbus/src/candidate.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct Candidate {
-    pub word: String,
-    pub annotation: String,
-    pub matched_len: Option<i32>,
-}
