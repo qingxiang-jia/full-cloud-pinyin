1aac3c3ec1424b96ee0b6ec7046bc578e6e04ca2
Qingxiang Jia <ybjqx3340@gmail.com>
2024-01-19 21:30:21 -0500

    Skaffold Nepali dispatcher

--- src/nepali/nepali_dispatcher.rs
+++ src/nepali/nepali_dispatcher.rs
@@ -8,13 +8,16 @@ use crate::common::{
     zmq::{Client, Server},
 };
 
-use super::{cloud_nepali::CloudNepali, nepali_decoder::NepaliDecoder};
+use super::{
+    cloud_nepali::CloudNepali, nepali_decoder::NepaliDecoder, number_service::NumberService,
+};
 
 pub struct NepaliDispatcher {
     zmq: Arc<Mutex<Client>>,
     nepali: CloudNepali<NepaliDecoder>,
     candidate_svc: CandidateService,
     preedit_svc: PreeditService,
+    number_svc: NumberService,
     level: Vec<usize>,
 }
 
@@ -26,12 +29,118 @@ impl Dispatcher for NepaliDispatcher {
             nepali: CloudNepali::new(),
             candidate_svc: CandidateService::new(req.clone()),
             preedit_svc: PreeditService::new(req.clone()),
+            number_svc: NumberService::new(req.clone()),
             level: vec![0],
         };
         dispatcher
     }
 
     async fn on_input(&self, key: FcitxKeySym, sock: &Server) {
+        match key {
+            FcitxKeySym::A
+            | FcitxKeySym::B
+            | FcitxKeySym::C
+            | FcitxKeySym::D
+            | FcitxKeySym::E
+            | FcitxKeySym::F
+            | FcitxKeySym::G
+            | FcitxKeySym::H
+            | FcitxKeySym::I
+            | FcitxKeySym::J
+            | FcitxKeySym::K
+            | FcitxKeySym::L
+            | FcitxKeySym::M
+            | FcitxKeySym::N
+            | FcitxKeySym::O
+            | FcitxKeySym::P
+            | FcitxKeySym::Q
+            | FcitxKeySym::R
+            | FcitxKeySym::S
+            | FcitxKeySym::T
+            | FcitxKeySym::U
+            | FcitxKeySym::V
+            | FcitxKeySym::W
+            | FcitxKeySym::X
+            | FcitxKeySym::Y
+            | FcitxKeySym::Z => _ = sock.send(false),
+            FcitxKeySym::a
+            | FcitxKeySym::b
+            | FcitxKeySym::c
+            | FcitxKeySym::d
+            | FcitxKeySym::e
+            | FcitxKeySym::f
+            | FcitxKeySym::g
+            | FcitxKeySym::h
+            | FcitxKeySym::i
+            | FcitxKeySym::j
+            | FcitxKeySym::k
+            | FcitxKeySym::l
+            | FcitxKeySym::m
+            | FcitxKeySym::n
+            | FcitxKeySym::o
+            | FcitxKeySym::p
+            | FcitxKeySym::q
+            | FcitxKeySym::r
+            | FcitxKeySym::s
+            | FcitxKeySym::t
+            | FcitxKeySym::u
+            | FcitxKeySym::v
+            | FcitxKeySym::w
+            | FcitxKeySym::x
+            | FcitxKeySym::y
+            | FcitxKeySym::z => {
+                // Tells the bridge that we accept this event.
+                _ = sock.send(true);
+                // Work on getting the candidates.
+                self.handle_nepali(key).await;
+            }
+            FcitxKeySym::Num0
+            | FcitxKeySym::Num1
+            | FcitxKeySym::Num2
+            | FcitxKeySym::Num3
+            | FcitxKeySym::Num4
+            | FcitxKeySym::Num5
+            | FcitxKeySym::Num6
+            | FcitxKeySym::Num7
+            | FcitxKeySym::Num8
+            | FcitxKeySym::Num9 => {
+                _ = sock.send(true);
+                if self.candidate_svc.in_session() {
+                    self.handle_select(key).await;
+                } else {
+                    self.number_svc.handle_number(key);
+                }
+            }
+            FcitxKeySym::Space
+            | FcitxKeySym::Return
+            | FcitxKeySym::Minus
+            | FcitxKeySym::Equal
+            | FcitxKeySym::Up
+            | FcitxKeySym::Down
+            | FcitxKeySym::Left
+            | FcitxKeySym::Right
+            | FcitxKeySym::Backspace
+            | FcitxKeySym::Escape => {
+                self.handle_control(key, sock).await;
+            }
+            FcitxKeySym::ShiftL | FcitxKeySym::ControlR | FcitxKeySym::AltL => {
+                _ = sock.send(false);
+            }
+            _ => _ = sock.send(false),
+        }
+    }
+}
+
+impl NepaliDispatcher {
+    async fn handle_nepali(&self, key: FcitxKeySym) {
+        todo!()
+    }
+
+    async fn handle_select(&self, key: FcitxKeySym) {
+        todo!()
+    }
+
+    async fn handle_control(&self, key: FcitxKeySym, sock: &Server) {
         todo!()
     }
 }
